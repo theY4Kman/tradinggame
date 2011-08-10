5f36306df0b7cfb17f136f3cbda773772f30615d
Andrew Miller <amiller@dappervision.com>
2011-08-09 23:20:28 -0400

    1) Moved to a flask server 2) Moved files into static/ 3) Added a redis config (needs to be modified for your machine)
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

--- server.py
+++ server.py
@@ -1,16 +1,50 @@
+import argparse
 import flask
-from werkzeug import SharedDataMiddleware
 from gevent.wsgi import WSGIServer
+import json
+import os
+import urlparse
+import redis
 
-app = flask.Flask('Trading Game')
+app = flask.Flask(__name__, static_path='/')
+#app.debug=True
+from werkzeug import SharedDataMiddleware
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/': os.path.join(os.path.dirname(__file__), 'static')})
+
+db = redis.Redis(port=8500)
+
+
+# Generate the secret key if it hasn't been already
+if db.get('secretkey') is None:
+    db.set('secretkey', os.urandom(36))
+app.secret_key = db.get('secretkey')
+
 
 @app.route('/')
-def index():
-  return flask.redirect('index.htm')
+def index(**kwargs):
+    return flask.redirect('/index.htm')
 
-app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
-  '/': '.'
-})
-app.debug = True
-http_server = WSGIServer(('localhost', 5000), app)
-http_server.serve_forever()
+
+@app.route('/test', methods=['GET'])
+def test():
+    return 'blah'
+
+
+@app.route('/post', methods=['POST'])
+def post():
+    # Get the JSON of a gameplay transaction and store it in the database
+    return 'OK'
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('BrowserID Chat Server')
+    parser.add_argument('--port', type=int, default=8091)
+    ARGS = parser.parse_args()
+
+    print 'Serving on port', ARGS.port
+    if app.debug:
+        app.run('0.0.0.0', ARGS.port)
+    else:
+        http_server = WSGIServer(('', ARGS.port), app)
+        http_server.serve_forever()
